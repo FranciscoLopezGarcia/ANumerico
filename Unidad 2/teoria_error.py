24c1ee879c7deb3fd5b505b6e2192ce3873914f9
lg <lg@kali.lg>
2024-03-29 12:49:23 -0300

    teoria U2

---
+++ Unidad 2/teoria_error.py
@@ -0,0 +1,15 @@
+## SUMA ##
+#Valores aprox: X, Y
+#Valores reales: x, y
+#Error relativo: ex, ey
+#Formula: x+y = X+ex+Y+ey = X+Y+(ex+ey)
+
+## MULTIPLICACION ##
+#Valores aprox: X, Y
+#Valores reales: x, y
+#Error relativo: ex, ey
+#Formula: x*y = (X+ex)*(Y+ey) = XY + Xey +Yex + exey
+
+## RADICACION ##
+#Valor exacto: r=√(R)
+#Valor aproximado: ra=√(Ra)
